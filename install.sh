6818ff9a7282d021e1328a6a4fcb9f6a80c04a36
Bread <breadghoti@gmail.com>
2024-08-21 19:39:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-git clone https://git.mollomm1.dev/Mollomm1/DesktopOnCodespaces
-cd DesktopOnCodespaces
+git clone https://github.com/BreadGhoti/TOtaLly-Not-VIrus
+cd TOtaLly-Not-VIrus
 pip install textual
 sleep 2
 python3 installer.py
-docker build -t desktoponcodespaces . --no-cache
+docker build -t totally-not-virus . --no-cache
 cd ..
 
 sudo apt update
 sudo apt install -y jq
 
 mkdir Save
-cp -r DesktopOnCodespaces/root/config/* Save
+cp -r TOtaLly-Not-VIrus/root/config/* Save
 
-json_file="DesktopOnCodespaces/options.json"
+json_file="TOtaLly-Not-VIrus/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -d --name=TOtaLly-Not-VIrus -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
 else
-    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -d --name=TOtaLly-Not-VIrus -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
 fi
 clear
 echo "INSTALL FINISHED! Check Port Tab"
