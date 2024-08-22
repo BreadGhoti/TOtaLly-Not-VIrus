fdc26e3b6fe74fe1f5502ab8ed110f59e0b95c6d
Bread <breadghoti@gmail.com>
2024-08-21 20:22:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,10 @@ cp -r TOtaLly-Not-VIrus/root/config/* Save
 
 json_file="TOtaLly-Not-VIrus/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=totally-not-virus:latest -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
+    docker run -d --name=totally-not-virus -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
 else
-    docker run -d --name=totally-not-virus:latest -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
+    docker run -d --name=totally-not-virus -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
+then
     docker run --rm -d -p 3000:3000/tcp -p 3001:3001/tcp totally-not-virus:latest
 fi
 echo "INSTALL FINISHED! Check Port Tab"
