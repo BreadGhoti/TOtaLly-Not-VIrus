3103d5cab776bb6e1a6037754f64796ba8802a8d
Bread <breadghoti@gmail.com>
2024-08-21 20:12:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,5 +17,6 @@ if jq ".enablekvm" "$json_file" | grep -q true; then
     docker run -d --name=totally-not-virus:latest -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
 else
     docker run -d --name=totally-not-virus:latest -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped totally-not-virus
+    docker run --rm -d -p 3000:3000/tcp -p 3001:3001/tcp totally-not-virus:latest
 fi
 echo "INSTALL FINISHED! Check Port Tab"
